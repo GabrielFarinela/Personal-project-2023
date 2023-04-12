6f8934c765188ccb1b940378088c039bd785058d
Gabriel Farinela <gabrielfarinela@hotmail.com>
2023-04-11 22:06:07 -0300

    final

--- DesenvolvimentoII/Ex4/js/master.js
+++ DesenvolvimentoII/Ex4/js/master.js
@@ -6,8 +6,52 @@ const valorTotal = document.getElementById("valor-total");
 const presente1 = document.getElementById("presente1");
 const presente2 = document.getElementById("presente2");
 
-const objLivro1 = { titulo: 'Engenharia de Software', autor: 'Engenharia de Software', preco: '194,24' };
-const objLivro2 = { titulo: 'Inteligência Artificial', autor: 'Stuart Russel, Peter Norvig', preco: '200,00' };
+const objLivro1 = { titulo: 'Engenharia de Software', src: 'img/sommerville_es.jpeg', autor: 'Ian Sommerville', valor: '194,24' };
+const objLivro2 = { titulo: 'Inteligência Artificial', src: 'img/norvig_ia.jpeg', autor: 'Stuart Russel, Peter Norvig', valor: '200,00' };
+
+function adicionaProduto() {
+  let novoArtigo = document.createElement("article");
+
+  novoArtigo.innerHTML = 
+  `<div class="produto" id="prod_1">
+  <div>
+      <img class="miniatura" src=${objLivro1.src} alt="Imagem do Produto">
+  </div>
+  <div class="nome">
+      <p>${objLivro1.titulo}</p>
+      <p class="autor">${objLivro1.autor}</p>
+  </div>
+  <div class="preco">
+      <p>R$ <span id="livro1" class="valor">${objLivro1.valor}</span></p>
+  </div>
+  <div class="quantidade">
+      <input onchange="escreveValorTotal()" id="quantidadeLivroAdd" type="number" name="quantidade" min="1" max="5" size="2" value="0">
+  </div>
+  <div class="presente">
+      <label>Para presente: </label>
+      <input id="presenteAdd" type="checkbox">
+  </div>
+  <button style="margin: 30px;" onclick="excluiProduto(this)">Excluir</button>
+</div>`
+
+  let artigoAnterior = document.querySelector("article:last-of-type");
+  artigoAnterior.parentNode.insertBefore(novoArtigo, artigoAnterior.nextSibling);
+}
+
+function excluiProduto(botao) {
+  let artigo = botao.parentNode.parentNode;
+  artigo.parentNode.removeChild(artigo);
+}
+
+function removerProdutos() {
+  let artigos = document.querySelectorAll("article");
+
+  for (let i = 1; i < artigos.length; i++) {
+    artigos[i].remove();
+  }
+
+  escreveValorTotal(0)
+}
 
 // arredonda valor
 function arred(d, casas) {
@@ -33,11 +77,10 @@ function calculaPrecoTotal() {
   const qtdLivro1 = quantidadeLivro1.value;
   const qtdLivro2 = quantidadeLivro2.value;
   const result = ((valorLivro1 * qtdLivro1) + (valorLivro2 * qtdLivro2))
-  return result
+  escreveValorTotal(result)
 }
 
-function escreveValorTotal() {
-  let result = calculaPrecoTotal()
+function escreveValorTotal(result) {
   if(presente1.checked && quantidadeLivro1.value > 0)
     result = result + 5
   if(presente2.checked  && quantidadeLivro2.value > 0)
